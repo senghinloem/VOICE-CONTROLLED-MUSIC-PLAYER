19a097de8208bd569cc0c8a6876e9c4bc03d5a1b
unknown <Sreynich.Chhoeurn@pnc.lan>
2024-11-18 14:09:34 +0700

    test

--- main.py
+++ main.py
@@ -4,7 +4,5 @@ import speech_recognition as sr
 import pygame
 import pyttsx3
 import random
-
+                 
 #================================================= MAIN =================================================#
-
-
