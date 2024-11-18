dc0c5cf1aa33159860f73873add6b65cb965adf7
Sokan <sokan.thol@student.passerellesnumeriques.org>
2024-11-18 14:41:49 +0700

    newclear

--- main.py
+++ main.py
@@ -4,12 +4,8 @@ import speech_recognition as sr
 import pygame
 import pyttsx3
 import random
-<<<<<<< HEAD
-                 
-=======
 from speech_recognition import *
 
->>>>>>> 3ea7babb32813fffc9eb29eb5972424c685c3f5a
 #================================================= MAIN =================================================#
 
 
